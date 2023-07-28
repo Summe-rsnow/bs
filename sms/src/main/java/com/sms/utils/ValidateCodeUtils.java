e826d6a3a42eab177c3ab967a42e04b3176e716a
sssnow <1543297674@qq.com>
2023-07-28 21:05:59 +0800

    重写了验证码生成工具类 为找回密码功能做准备

--- sms/src/main/java/com/sms/utils/ValidateCodeUtils.java
+++ sms/src/main/java/com/sms/utils/ValidateCodeUtils.java
@@ -6,40 +6,31 @@ import java.util.Random;
  * 随机生成验证码工具类
  */
 public class ValidateCodeUtils {
+    private static final int DEFAULT_LENGTH = 4; // 默认验证码长度为4位
+    private static final char[] NUMERIC_CHARS = "0123456789".toCharArray();
+
     /**
-     * 随机生成验证码
-     *
-     * @param length 长度为4位或者6位
+     * 生成指定长度的纯数字验证码
+     * @param length
      * @return
      */
-    public static Integer generateValidateCode(int length) {
-        Integer code = null;
-        if (length == 4) {
-            code = new Random().nextInt(9999);//生成随机数，最大为9999
-            if (code < 1000) {
-                code = code + 1000;//保证随机数为4位数字
-            }
-        } else if (length == 6) {
-            code = new Random().nextInt(999999);//生成随机数，最大为999999
-            if (code < 100000) {
-                code = code + 100000;//保证随机数为6位数字
-            }
-        } else {
-            throw new RuntimeException("只能生成4位或6位数字验证码");
+    public static String generate(int length) {
+        if (length <= 0) {
+            throw new IllegalArgumentException("验证码长度必须大于0");
         }
-        return code;
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(NUMERIC_CHARS[random.nextInt(NUMERIC_CHARS.length)]);
+        }
+        return sb.toString();
     }
 
     /**
-     * 随机生成指定长度字符串验证码
-     *
-     * @param length 长度
+     * 生成默认长度的纯数字验证码
      * @return
      */
-    public static String generateValidateCode4String(int length) {
-        Random rdm = new Random();
-        String hash1 = Integer.toHexString(rdm.nextInt());
-        String capstr = hash1.substring(0, length);
-        return capstr;
+    public static String generate() {
+        return generate(DEFAULT_LENGTH);
     }
 }
