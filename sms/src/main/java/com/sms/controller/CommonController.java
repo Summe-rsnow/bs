cff9d5f74fa25763b12789e0aa5c987724da34d7
sssnow <1543297674@qq.com>
2023-07-28 22:26:01 +0800

    优化头像上传下载相关代码 改正注释

--- sms/src/main/java/com/sms/controller/CommonController.java
+++ sms/src/main/java/com/sms/controller/CommonController.java
@@ -1,9 +1,7 @@
 package com.sms.controller;
 
 import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.lang.Snowflake;
-import cn.hutool.core.util.IdUtil;
-import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import cn.hutool.core.io.IoUtil;
 import com.sms.common.BaseContext;
 import com.sms.common.Result;
 import com.sms.entity.User;
@@ -34,15 +32,6 @@ public class CommonController {
     @Value("${sms.path}")
     private String basePath;
 
-    private static String getSuffix(MultipartFile file) {
-        String suffix = null;
-        String originalFilename = file.getOriginalFilename();
-        if (originalFilename != null) {
-            suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
-        }
-        return suffix;
-    }
-
     private static void responseHeader(HttpServletResponse response) {
         response.setContentType("image/jpeg");
         response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
@@ -53,21 +42,11 @@ public class CommonController {
     //头像上传接口
     @PostMapping("/avatar/upload")
     public Result<String> upload(MultipartFile file) throws IOException {
-        String fileName;
-        String path;
         //获取文件原始文件名,并获文件后缀
         User user = userService.getById(BaseContext.getCurrentId());
         String userAvatar = user.getAvatar();
-        if (userAvatar == null || userAvatar.isEmpty()) {
-            String suffix = getSuffix(file);
-            //使用雪花算法生成头像名称 防止头像名称碰撞覆盖
-            Snowflake snowflake = IdUtil.getSnowflake(1, 1);
-            fileName = snowflake.nextIdStr() + suffix;
-            updateAvatar(fileName);
-        } else {
-            fileName = userAvatar;
-        }
-        path = basePath + "\\" + userAvatar;
+        String fileName = userAvatar;
+        String path = basePath + "\\" + userAvatar;
         saveAvatar(file, path, fileName);
         return Result.success(fileName);
     }
@@ -80,56 +59,29 @@ public class CommonController {
         log.info("已存储文件{}至{}", fileName, basePath);
     }
 
-    private void updateAvatar(String fileName) {
-        User user = userService.getById(BaseContext.getCurrentId());
-        user.setAvatar(fileName);
-        LambdaUpdateWrapper<User> wrapper = new LambdaUpdateWrapper<>();
-        wrapper.eq(User::getId, user.getId());
-        userService.update(user, wrapper);
-    }
-
     //头像下载接口
     @GetMapping("/avatar/download")
-    public void avatar(HttpServletResponse response) {
+    public void avatar(HttpServletResponse response) throws IOException {
         User user = userService.getById(BaseContext.getCurrentId());
         String userAvatar = user.getAvatar();
-        if (userAvatar == null || userAvatar.isEmpty()) {
-            userAvatar = "1.png";
-        }
         String fielname = basePath + "\\" + userAvatar;
         if (!FileUtil.exist(fielname)) {
             fielname = basePath + "\\1.png";
         }
-        log.info("需要下的文件名称为：{}", fielname);
-        //设置相应头 让头像不缓存 确保切换用户时能刷新头像
+        log.info("需要下载的文件名称为：{}", fielname);
+        //设置响应头 让头像不缓存 确保切换用户时能刷新头像
         responseHeader(response);
-        try {
-            //输入流，通过输入流读取文件内容
-            FileInputStream fileInputStream = new FileInputStream(fielname);
-            //输出流，将图片写回浏览器
-            ServletOutputStream servletOutputStream = response.getOutputStream();
-            int len;
-            byte[] bytes = new byte[2048];
-            while ((len = fileInputStream.read(bytes)) != -1) {
-                servletOutputStream.write(bytes, 0, len);
-                servletOutputStream.flush();
-            }
-            servletOutputStream.close();
-            fileInputStream.close();
-        } catch (Exception e) {
-            log.error(String.valueOf(e));
-        }
+        //输入流，通过输入流读取文件内容
+        FileInputStream fileInputStream = new FileInputStream(fielname);
+        //输出流，将图片写回浏览器
+        ServletOutputStream servletOutputStream = response.getOutputStream();
+        byte[] bytes = IoUtil.readBytes(fileInputStream);
+        IoUtil.write(servletOutputStream,true,bytes);
     }
 
     //验证码返回接口
     @GetMapping("/vcode")
     public void vcode(HttpServletResponse response) throws IOException {
-        /*
-        外部工具类实现
-        LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(140, 37);
-        lineCaptcha.createCode();
-        String vcode = lineCaptcha.getCode();*/
-
         //定义图形验证码的长，宽和长度
         ValidateCodePicUtils.ValidateCodePic validateCodePic = ValidateCodePicUtils.create(140, 37, 4);
         String code = validateCodePic.getCode();
